b7a777688d32ed5a00ae501f906c7f58726e1374
okipm <rzcortex@gmail.com>
2026-02-16 23:19:33 +0800

    app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def get_employee_display_name(emp_id, df):
 
 def login():
     """Login page"""
-    st.markdown('<div class="main-header">🔐 HR PRO LOGIN</div>', unsafe_allow_html=True)
+    st.markdown('<div class="main-header">🔐 HR Management System</div>', unsafe_allow_html=True)
     
     col1, col2, col3 = st.columns([1, 2, 1])
     
