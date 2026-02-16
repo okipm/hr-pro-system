b14ab97d61283128fb62a8e4193d85aec64488d9
okipm <rzcortex@gmail.com>
2026-02-16 23:17:52 +0800

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from io import BytesIO
 # =====================================================
 
 st.set_page_config(
-    page_title="HR PRO SYSTEM",
+    page_title="HR Management System",
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={
